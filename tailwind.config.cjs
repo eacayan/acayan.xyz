a9149028548e9abcc6bc4c53929146d22127f7c1
Ellice Acayan <acayanellice@gmail.com>
2022-10-17 20:42:49 +0800

    home page changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,17 @@ module.exports = {
 			fontFamily: {
 				sans: ["Space Grotesk", ...defaultTheme.fontFamily.sans],
 			},
+			keyframes: {
+				scroll: {
+					to: {
+						opacity: 0,
+						bottom: "48px",
+					},
+				},
+			},
+			animation: {
+				scroll: "scroll 1500ms infinite",
+			},
 		},
 	},
 	plugins: [
@@ -22,7 +33,7 @@ module.exports = {
 					"@apply w-6 h-6 md:w-8 md:h-8 transition-all duration-300 ease-in-out text-white hover:text-rose-500": {},
 				},
 				".gradient-bg": {
-					"@apply before:absolute before:w-[130%] before:h-[60vh] before:top-[0] before:-left-[90px] before:-z-10 md:before:h-[95vh] md:before:-left-[120px]":
+					"@apply before:absolute before:w-[130%] before:h-[60vh] before:top-[0] before:-left-[90px] before:-z-10 md:before:w-full md:before:h-[95vh] md:before:left-0":
 						{},
 					"&::before": {
 						backgroundImage: `radial-gradient(at 65% 75%, ${theme(
